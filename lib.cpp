defcc18861d594f23bf5b08ab091612c15e62907
Blindello <129384662+Blindello@users.noreply.github.com>
2023-05-25 18:05:06 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "Libreria.h"
 
+float funzione1 (float x[], int n) {
+    float f;
+    float max=x[0];
+    for (int i=0; i<n; i++){
+        if (x[i]>max) {
+            max=x[i];
+    }
+    }
+    return max;
+}
