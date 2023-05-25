11a3a27f0a75cc495d3a3261db8fada7e058fe27
Blindello <129384662+Blindello@users.noreply.github.com>
2023-05-25 18:04:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "Libreria.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    int n;
+    float f;
+    cout<<"inserisci dimensione array"<<endl;
+    cin>>n;
+    float numeri[n];
+    for (int i=0; i<n; i++) {
+        cin>>numeri[i];
+    }
+    f=funzione1(numeri, n);
+    cout<<f<<endl;
+    return 0;
 }
